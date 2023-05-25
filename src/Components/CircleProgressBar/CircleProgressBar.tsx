339f8ff6d1e14195ef044c3f074a14c003d70249
nat-davydova <natti.davydova@gmail.com>
2023-05-25 18:02:22 +0300

    feat: add basic circle

--- src/Components/CircleProgressBar/CircleProgressBar.tsx
+++ src/Components/CircleProgressBar/CircleProgressBar.tsx
@@ -3,7 +3,16 @@ import styles from "./CircleProgressBar.module.css";
 export function CircleProgressBar() {
   return (
     <div className={styles.container}>
-      <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg"></svg>
+      <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+        <circle
+          cx={50}
+          cy={50}
+          r={45}
+          strokeWidth={5}
+          stroke="black"
+          fill="transparent"
+        />
+      </svg>
     </div>
   );
 }
