6fd933baddb6afa1888816f257ac6dfd91a62637
nat-davydova <natti.davydova@gmail.com>
2023-05-25 18:08:49 +0300

    refactor: move circle center coords into separate var and add default stroke

--- src/Components/CircleProgressBar/CircleProgressBar.tsx
+++ src/Components/CircleProgressBar/CircleProgressBar.tsx
@@ -1,14 +1,17 @@
 import styles from "./CircleProgressBar.module.css";
 
+const PROGRESS_STROKE_WIDTH_DEFAULT_PX = 3;
+const CIRCLE_CENTER_COORD = 50;
+
 export function CircleProgressBar() {
   return (
     <div className={styles.container}>
       <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
         <circle
-          cx={50}
-          cy={50}
+          cx={CIRCLE_CENTER_COORD}
+          cy={CIRCLE_CENTER_COORD}
           r={45}
-          strokeWidth={5}
+          strokeWidth={PROGRESS_STROKE_WIDTH_DEFAULT_PX}
           stroke="black"
           fill="transparent"
         />
