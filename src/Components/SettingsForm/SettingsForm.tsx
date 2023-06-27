3703bb0f296d44c913f0d490dd88ebb08120ee86
nat-davydova <natti.davydova@gmail.com>
2023-06-27 19:56:56 +0300

    feat: add surface fields

--- src/Components/SettingsForm/SettingsForm.tsx
+++ src/Components/SettingsForm/SettingsForm.tsx
@@ -25,6 +25,14 @@ export function SettingsForm() {
           <label htmlFor="caps-square">Square: </label>
           <input id="caps-square" type="radio" name="caps" value="square" />
         </div>
+        <div>
+          <h3>Surface</h3>
+          <label htmlFor="show-surface">Show Surface: </label>
+          <input id="show-surface" type="checkbox" value="show" checked />
+
+          <label htmlFor="surface-color">Surface Color: </label>
+          <input id="surface-color" type="color" value="#e3e3e3" />
+        </div>
       </form>
     </div>
   );
