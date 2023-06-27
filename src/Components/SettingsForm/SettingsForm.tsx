ef7b81b6c22810799b3a3c8b2de08392ea25259e
nat-davydova <natti.davydova@gmail.com>
2023-06-27 19:42:39 +0300

    feat: add basic settings content

--- src/Components/SettingsForm/SettingsForm.tsx
+++ src/Components/SettingsForm/SettingsForm.tsx
@@ -1,3 +1,8 @@
 export function SettingsForm() {
-  return <div>Form</div>;
+  return (
+    <div>
+      <h2>Pick Progress Bar Settings</h2>
+      <form name="progress-settings"></form>
+    </div>
+  );
 }
