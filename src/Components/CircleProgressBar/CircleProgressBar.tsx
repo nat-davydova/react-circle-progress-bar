3d64e99c582d529e77a29d6268e0d29150201b1a
nat-davydova <natti.davydova@gmail.com>
2023-06-27 18:44:18 +0300

    feat: add textLabel types support

--- src/Components/CircleProgressBar/CircleProgressBar.tsx
+++ src/Components/CircleProgressBar/CircleProgressBar.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 import styles from "./CircleProgressBar.module.css";
 import {
   CIRCLE_CENTER_COORD,
@@ -18,6 +20,7 @@ interface ICircleProgressBarProps {
   strokeLineCap?: TStrokeLineCaps;
   progressBar: IProgressBarSettings;
   containerClassName?: string;
+  textLabel?: React.ReactElement | string;
 }
 
 export function CircleProgressBar({
