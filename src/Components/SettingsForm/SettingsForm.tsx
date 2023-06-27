d05f988a0f399d64abe8a426a969eed75deed717
nat-davydova <natti.davydova@gmail.com>
2023-06-27 19:49:15 +0300

    feat: add percentage and strokeWidth fields

--- src/Components/SettingsForm/SettingsForm.tsx
+++ src/Components/SettingsForm/SettingsForm.tsx
@@ -2,7 +2,16 @@ export function SettingsForm() {
   return (
     <div>
       <h2>Pick Progress Bar Settings</h2>
-      <form name="progress-settings"></form>
+      <form name="progress-settings">
+        <div>
+          <label htmlFor="percentage">Percentage:</label>
+          <input id="percentage" type="number" min={0} max={100} value={65} />
+        </div>
+        <div>
+          <label htmlFor="strokeWidth">Stroke width:</label>
+          <input id="strokeWidth" type="number" min={1} max={50} value={4} />
+        </div>
+      </form>
     </div>
   );
 }
