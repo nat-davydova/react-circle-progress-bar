c271333cc455a0ff12765e26d27c76ace8ae8344
nat-davydova <natti.davydova@gmail.com>
2023-06-27 19:52:39 +0300

    feat: add caps field

--- src/Components/SettingsForm/SettingsForm.tsx
+++ src/Components/SettingsForm/SettingsForm.tsx
@@ -11,6 +11,20 @@ export function SettingsForm() {
           <label htmlFor="strokeWidth">Stroke width:</label>
           <input id="strokeWidth" type="number" min={1} max={50} value={4} />
         </div>
+        <div>
+          <h3>Stroke Line Caps</h3>
+          <label htmlFor="caps-round">Round: </label>
+          <input
+            id="caps-round"
+            type="radio"
+            name="caps"
+            value="round"
+            checked
+          />
+
+          <label htmlFor="caps-square">Square: </label>
+          <input id="caps-square" type="radio" name="caps" value="square" />
+        </div>
       </form>
     </div>
   );
