113379396a4f5b631ee91dcc117ff398cec0e71a
nat-davydova <natti.davydova@gmail.com>
2023-07-05 19:56:48 +0300

    feat: add form field blocks

--- src/Components/SettingsForm/SettingsForm.tsx
+++ src/Components/SettingsForm/SettingsForm.tsx
@@ -5,89 +5,102 @@ export function SettingsForm() {
     <div>
       <h2>Pick Progress Bar Settings</h2>
       <form className={styles.form} name="progress-settings">
-        <div className={styles.field}>
-          <label htmlFor="percentage">Percentage:</label>
-          <input id="percentage" type="number" min={0} max={100} value={65} />
+        <div className={styles.fieldsContainer}>
+          <div className={styles.field}>
+            <label htmlFor="percentage">Percentage:</label>
+            <input id="percentage" type="number" min={0} max={100} value={65} />
+          </div>
+          <div className={styles.field}>
+            <label htmlFor="strokeWidth">Stroke width:</label>
+            <input id="strokeWidth" type="number" min={1} max={50} value={4} />
+          </div>
         </div>
-        <div className={styles.field}>
-          <label htmlFor="strokeWidth">Stroke width:</label>
-          <input id="strokeWidth" type="number" min={1} max={50} value={4} />
-        </div>
+
         <div>
           <h3>Stroke Line Caps</h3>
 
-          <div className={styles.field}>
-            <label htmlFor="caps-round">Round: </label>
-            <input
-              id="caps-round"
-              type="radio"
-              name="caps"
-              value="round"
-              checked
-            />
-          </div>
+          <div className={styles.fieldsContainer}>
+            <div className={styles.field}>
+              <label htmlFor="caps-round">Round: </label>
+              <input
+                id="caps-round"
+                type="radio"
+                name="caps"
+                value="round"
+                checked
+              />
+            </div>
 
-          <div className={styles.field}>
-            <label htmlFor="caps-square">Square: </label>
-            <input id="caps-square" type="radio" name="caps" value="square" />
+            <div className={styles.field}>
+              <label htmlFor="caps-square">Square: </label>
+              <input id="caps-square" type="radio" name="caps" value="square" />
+            </div>
           </div>
         </div>
         <div>
           <h3>Surface</h3>
-          <div className={styles.field}>
-            <label htmlFor="show-surface">Show Surface: </label>
-            <input id="show-surface" type="checkbox" value="show" checked />
-          </div>
 
-          <div className={styles.field}>
-            <label htmlFor="surface-color">Surface Color: </label>
-            <input id="surface-color" type="color" value="#e3e3e3" />
+          <div className={styles.fieldsContainer}>
+            <div className={styles.field}>
+              <label htmlFor="show-surface">Show Surface: </label>
+              <input id="show-surface" type="checkbox" value="show" checked />
+            </div>
+
+            <div className={styles.field}>
+              <label htmlFor="surface-color">Surface Color: </label>
+              <input id="surface-color" type="color" value="#e3e3e3" />
+            </div>
           </div>
         </div>
         <div>
           <h3>Progress Bar</h3>
-          <div className={styles.field}>
-            <label htmlFor="progress-bg">Plain Background: </label>
-            <input
-              id="progress-bg"
-              type="radio"
-              name="progress"
-              value="bg"
-              checked
-            />
-          </div>
 
-          <div className={styles.field}>
-            <label htmlFor="progress-gradient">Gradient: </label>
-            <input
-              id="progress-gradient"
-              type="radio"
-              name="progress"
-              value="gradient"
-            />
-          </div>
+          <div className={styles.fieldsBlock}>
+            <div className={styles.fieldsContainer}>
+              <div className={styles.field}>
+                <label htmlFor="progress-bg">Plain Background: </label>
+                <input
+                  id="progress-bg"
+                  type="radio"
+                  name="progress"
+                  value="bg"
+                  checked
+                />
+              </div>
 
-          <div className={styles.field}>
-            <label htmlFor="progress-color">Progress Color: </label>
-            <input id="progress-color" type="color" value="#fff000" />
-          </div>
-
-          <div>
-            <div className={styles.field}>
-              <label htmlFor="percent-0">Percent: </label>
-              <input
-                id="percent-0"
-                type="number"
-                min={0}
-                max={100}
-                value={65}
-              />
+              <div className={styles.field}>
+                <label htmlFor="progress-gradient">Gradient: </label>
+                <input
+                  id="progress-gradient"
+                  type="radio"
+                  name="progress"
+                  value="gradient"
+                />
+              </div>
             </div>
+
             <div className={styles.field}>
-              <label htmlFor="color-0">Color: </label>
-              <input id="color-0" type="color" value="#cccbbb" />
+              <label htmlFor="progress-color">Progress Color: </label>
+              <input id="progress-color" type="color" value="#fff000" />
             </div>
-            <button>Add Point</button>
+
+            <div className={styles.fieldsContainer}>
+              <div className={styles.field}>
+                <label htmlFor="percent-0">Percent: </label>
+                <input
+                  id="percent-0"
+                  type="number"
+                  min={0}
+                  max={100}
+                  value={65}
+                />
+              </div>
+              <div className={styles.field}>
+                <label htmlFor="color-0">Color: </label>
+                <input id="color-0" type="color" value="#cccbbb" />
+              </div>
+              <button>Add Point</button>
+            </div>
           </div>
         </div>
         <div>
